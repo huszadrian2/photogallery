e64ff3dbc1184b61fecfcbde8bc131b8a3b308b3
Adrian Husz <coder@rianfoto.com>
2019-11-01 21:28:42 +0100

    Visszamenőleges javítások

--- resources/views/gallery/create.blade.php
+++ resources/views/gallery/create.blade.php
@@ -15,7 +15,22 @@
             </div>
           </div>
           <div class="row small-up-2 medium-up-3 large-up-4">
-            <div class="margo">Új képgaléria készítése</div>
+            <div class="margo">
+              {!! Form::open(['action' => 'GalleryController@store', 'enctype' => 'multipart/form-data']) !!}
+
+                  {!! Form::label('nev','Név') !!}
+                  {!! Form::text('nev', $value=null, $attributes=['placeholder'=>'Galéria neve', 'name'=>'nev']) !!}
+
+                  {!! Form::label('leiras','Leírás') !!}
+                  {!! Form::text('leiras', $value=null, $attributes=['placeholder'=>'Galéria leírása', 'name'=>'leiras']) !!}
+
+                  {!! Form::label('boritokep','Borítókép') !!}
+                  {!! Form::file('boritokep') !!}
+
+                  {!! Form::submit('Létrehozás', $attributes=['class'=>'button']) !!}
+                  
+              {!! Form::close() !!}
+            </div>
           </div>
 @stop
 
